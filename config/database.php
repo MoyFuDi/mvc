a23862f335be834f1176af38a2db9dd85ce547cc
Moises Fuentes Dimas <dfuentes@univim.edu.mx>
2019-05-20 23:37:29 -0500

    Inicio del proyecto. Se creo la estructura del proyecto para mvc. Ya contiene codigo inicial en algunos archivos.

---
+++ config/database.php
@@ -0,0 +1,9 @@
+<?php
+    return array(
+        "driver" => "mysql",
+        "host"   => "localhost",
+        "user"   => "admin",
+        "pass"   => "Sup3rAdm1n.",
+        "database" => "pruebas",
+        "charset"  => "utf8"
+    );
