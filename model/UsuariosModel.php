584fccbc5a97db833d6780986a1c8051b5378307
Moises Fuentes Dimas <dfuentes@univim.edu.mx>
2019-05-22 14:49:54 -0500

    Se utiliza para las consultas completas en lugar de usar los metodos que se tienen en el modelo entidad.

---
+++ model/UsuariosModel.php
@@ -0,0 +1,18 @@
+<?php
+	class UsuariosModel extends ModeloBase{
+		private $table;
+
+		public function __construct(){
+			$this->table="usuarios";
+			parent::__construct($this->table);
+		}
+
+		//Métodos de consulta
+		public function getUnUsuario(){
+			$query = "SELECT * FROM usuarios WHERE email='victor@victor.com'";
+			$usuario = $this->ejecutarSql($query);
+			return $usuario;
+		}
+
+	}
+	
